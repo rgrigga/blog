6420ec60be35b4be0357c0397e4fb1ac2245931c
Ryan Grissinger <ryan.grissinger@gmail.com>
2014-01-20 02:01:26 -0500

    composer

---
+++ src/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('blog::templates.scaffold')
+@section('main')
+<h1>Database</h1>
+
+<h2>Push Schema</h2>
+<a href="#" class="btn btn-success">Push</a>
+<h2>Pull Schema</h2>
+<a href="#" class="btn btn-success">Pull</a>
+@stop
+@section('footer')
+<footer>
+	<p>Thank you for your time and attention.</p>
+</footer>
+@stop
